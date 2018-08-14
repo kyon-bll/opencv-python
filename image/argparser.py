c6d88539ae75f4a61cf8d3ca064bda3b8e1d46f2
kyosuke <kyosuke.araki611@gmail.com>
2018-08-15 01:36:41 +0900

    make image.argparser.add_parser function

--- image/argparser.py
+++ image/argparser.py
@@ -4,9 +4,15 @@ parser = argparse.ArgumentParser(description='Edit img and Output image in `outp
 
 subparsers = parser.add_subparsers(dest='subcommand')
 
-parser_rotate = subparsers.add_parser('rotate', help='rotate img by degree')
-parser_rotate.add_argument('input_img',  help='input image path')
+def add_subparser(subcommand, **kwargs):
+    subparser = subparsers.add_parser(subcommand, **kwargs)
+    subparser.add_argument('input_img', help='input image path')
+
+    return subparser
+
+
+parser_rotate = add_subparser('rotate', help='rotate img by degree')
 parser_rotate.add_argument('degree', type=float, help='can accept float')
 
-parser_zoom = subparsers.add_parser('zoom', help='zoom img')
+parser_zoom = add_subparser('zoom', help='zoom img')
 parser_zoom.add_argument('scale', type=float, help='scale int times')
